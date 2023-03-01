b1bb4670e4fc9686dd35a72f650f895d9c8e7031
Nott - Nattaporn Chatmalairut <nattaporn.cha@everapp.io>
2023-03-01 15:25:34 +0700

    add solution for problem `Solve me first`.

---
+++ src/test/scala/me/solve/hacker_rank/solve_me_first/SolveMeFirstSpec.scala
@@ -0,0 +1,7 @@
+package me.solve.hacker_rank.solve_me_first
+
+class SolveMeFirstSpec extends munit.FunSuite {
+  test("solve me first") {
+    assertEquals(SolveMeFirst.solveMeFirst(List("1", "2")), 3)
+  }
+}
